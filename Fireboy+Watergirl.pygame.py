12e00fe0fe7a8d418d1049b51b9473a54369cdce
IslaSimpson3 <156451853+IslaSimpson3@users.noreply.github.com>
2024-01-13 15:51:44 +0000

    Creating different lands and boundaries
    for the two players, so they don't leave the game

--- Fireboy+Watergirl.pygame.py
+++ Fireboy+Watergirl.pygame.py
@@ -1,6 +1,8 @@
 import pygame 
 from pygame.locals import *
 
+tile_size = 50
+
 class Player1:
     def __init__(self, x, y):
         # Load and resize and posistioning the icecube
@@ -15,19 +17,22 @@ class Player1:
 
    #key movements for x and y coordinates     
     def move_right1(self):
-        self.rect.x += 3 # the icecube will move 3 pixels right . forward = + backward = - 
+        if self.rect.right < max_x: #boundry 
+            self.rect.x += 3  # the icecube will move 3 pixels right . forward = + backward = - 
              
     def move_left1(self):
-        self.rect.x -= 3 
+        if self.rect.left > min_x:
+            self.rect.x -= 3 
         
     def move_down1(self):
-        self.rect.y  #so the game doesnt end when someone presses the down key 
+         self.rect.y  #so the game doesnt end when someone presses the down key 
     
     def jump1(self):
        #if the icecube is on the ground then the icecubec
        while self.onGround:
             self.velocityY = -10  # up = -
             self.onGround = False
+           
             
     def update1(self):
         self.velocityY += 0.5 #amount the icecube can jump (how weak is the gravity)
@@ -52,13 +57,21 @@ class Player2:
         #self.rect.y = 0 #up
         self.velocityY = 0  #for when the ice cube jumps
         self.onGround = True 
+        if self.rect.x > 1000:
+            self.rect.x = 1000
+        else:
+            self.rect.x < 0
+            self.rect.x = 0
+
 
    #key movements for x and y coordinates     
     def move_right2(self):
-        self.rect.x += 3 # the icecube will move 3 pixels right . forward = + backward = - 
+       if self.rect.right < max_x:
+           self.rect.x += 3  # the icecube will move 3 pixels right . forward = + backward = - 
              
     def move_left2(self):
-        self.rect.x -= 3 
+        if self.rect.left > min_x:
+            self.rect.x -= 3 
         
     def move_down2(self):
         self.rect.y  #so the game doesnt end when someone presses the down key 
@@ -82,9 +95,81 @@ class Player2:
         surface.blit(self.image, self.rect) #show on screen(surface)
 
 
+class Lands():
+   def __init__(self,data):
+        self.tile_list = []    #empty list    
+
+        rock_img = pygame.image.load('./rocks.png')
+        lava_img = pygame.image.load('./lava.png')
+        water_img = pygame.image.load('./water.png')
+        grass_img = pygame.image.load('./grass.png')
+
+
+        row_count = 0  #row and collum count for steps = 0 
+        for row in data: #looking at the environment_data per row and then within a row per tile 
+            collum_count = 0
+            for tile in row:
+                if tile == 1: #if the tile = 1 meaning there is a step then the step image is scaled to the tile size 
+                    img = pygame.transform.scale(rock_img, (tile_size, tile_size)) #would be 50 by 50 
+                    scaled_image = img.get_rect() #turned a scaled image into a rectangle an stored in scaled image varable 
+                    scaled_image.x = collum_count * tile_size #collum count and tile size = the x cordinate of tiles top left corner 
+                    scaled_image.y = row_count * tile_size 
+                    tile = (img, scaled_image)  # tile = the info of img and scaled_image
+                    self.tile_list.append(tile) # this is then added to the empty list 
+                if tile == 2:  
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))  
+                    scaled_image = img.get_rect()  
+                    scaled_image.x = collum_count * tile_size
+                    scaled_image.y = row_count * tile_size 
+                    tile = (img, scaled_image)  
+                    self.tile_list.append(tile) 
+                if tile == 3: 
+                    img = pygame.transform.scale(lava_img, (tile_size, tile_size)) 
+                    scaled_image = img.get_rect()
+                    scaled_image.x = collum_count * tile_size 
+                    scaled_image.y = row_count * tile_size 
+                    tile = (img, scaled_image)  
+                    self.tile_list.append(tile)
+                if tile == 4: 
+                    img = pygame.transform.scale(water_img, (tile_size, tile_size)) 
+                    scaled_image = img.get_rect()  
+                    scaled_image.x = collum_count * tile_size 
+                    scaled_image.y = row_count * tile_size 
+                    tile = (img, scaled_image)  
+                    self.tile_list.append(tile) 
+                collum_count += 1
+            row_count += 1
+   def draw(self, screen):
+       for tile in self.tile_list:
+        screen.blit(tile[0], tile[1])
+
+        
+environment_data = [
+[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 
+[2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], 
+[2, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 3, 4, 2, 2, 0, 0, 0, 2], 
+[2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 2, 2, 2], 
+[2, 0, 0, 2, 0, 0, 0, 0, 0, 2, 3, 3, 4, 4, 0, 0, 0, 0, 0, 2], 
+[2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 2], 
+[2, 2, 2, 2, 3, 3, 2, 2, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 2], 
+[2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 4, 4, 0, 2, 0, 0, 0, 0, 0, 2], 
+[2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2], 
+[2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 4, 4, 2, 0, 0, 0, 0, 0, 0, 2], 
+[2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 2], 
+[2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 0, 0, 0, 2], 
+[2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 2], 
+]
+
+world = Lands(environment_data)
+
+
 pygame.init()   
 screen_width = 1000
 screen_height = 650
+min_x = tile_size # Left boundary
+min_y = tile_size  # Top boundary
+max_x = screen_width - tile_size  # Right boundary
+max_y = screen_height  
 
 screen = pygame.display.set_mode((screen_width, screen_height)) 
 pygame.display.set_caption('Platformer')
@@ -96,11 +181,10 @@ player2 = Player2(10,10)
 run = True
 while run:  # code of game keeps looping
     screen.blit(bg_img, (0, 0))  # shows background image onto the screen
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # closes the game
             run = False
-            
+              
     userInput1 = pygame.key.get_pressed()
     if userInput1[pygame.K_LEFT]:
         player1.move_left1()
@@ -120,12 +204,14 @@ while run:  # code of game keeps looping
         player2.move_down2()
     if userInput2[pygame.K_SPACE]:
         player2.jump2()
-        
+
+    world.draw(screen)     
     player1.update1()
     player1.draw1(screen) 
     player2.update2()
     player2.draw2(screen) # Draw the characte
     pygame.display.flip()  # update the display
+    
 
 pygame.quit()
 
