a6d4a371e800aadee37a3dd4cfe2da2eaf311645
IslaSimpson3 <156451853+IslaSimpson3@users.noreply.github.com>
2024-02-01 09:59:25 +0000

    Collision detection for lands with functions
    Created collision with the moving rocks. Also when the ice cube touches the lava land it will die, and when the fire touches the water it dies. I have been trying to create a game over screen and an intro screen but it isn't working...yet.

--- Fireboy+Watergirl.pygame.py
+++ Fireboy+Watergirl.pygame.py
@@ -1,24 +1,30 @@
-# Resources used:
-# https://www.geeksforgeeks.org/pygame-tutorial/?ref=lbp
-# https://www.youtube.com/watch?v=W_JRd3ntyBg&list=PLjcN1EyupaQnHM1I9SmiXfbT6aG4ezUvu&index=2 
-# https://www.youtube.com/watch?v=W_JRd3ntyBg&list=PLjcN1EyupaQnHM1I9SmiXfbT6aG4ezUvu&index=2
+# RESOURCES USED:
+# https://www.geeksforgeeks.org/pygame-tutorial/?ref=lbp with the introduction to pygame 
+# https://www.youtube.com/watch?v=W_JRd3ntyBg&list=PLjcN1EyupaQnHM1I9SmiXfbT6aG4ezUvu&index=2 creating the players 
+# https://www.youtube.com/watch?v=W_JRd3ntyBg&list=PLjcN1EyupaQnHM1I9SmiXfbT6aG4ezUvu&index=2 creating the environment_data (tiles for lands)
 # https://www.youtube.com/watch?v=1_H7InPMjaY 
 # https://www.w3schools.com/python/python_inheritance.asp for the inheritance 
 # https://www.youtube.com/watch?v=vz8YnvnAPp4 intro screen 
 # https://www.youtube.com/watch?v=qbkj81_BOes adding dimonds 
+# https://www.youtube.com/watch?v=1_H7InPMjaY&t=677s collision detection for moving rocks 
+# https://www.youtube.com/watch?v=1_H7InPMjaY&t=677s collison on static and moving objects (the rocks)
+# chatGPT for parts of the collisions, intro and gameover screen (noted in the code)
+# https://www.youtube.com/watch?v=G8VsEbVS3F8&list=PLjcN1EyupaQnHM1I9SmiXfbT6aG4ezUvu&index=6 game over if players touch either water or lava 
+# https://www.youtube.com/watch?v=yRkyrOWmSl4 game over screen 
 
 import pygame 
 from pygame.locals import *
 import sys
 
 tile_size = 50
-
+#attempt to create an intro page, doesnt display for very long 
+#asked chatGPT for help but the screen still couldnt be displayed 
 def intro_page(screen):
     intro = True 
     font = pygame.font.Font(None, 50)
     intro_image = pygame.image.load('./intro_page.png')
-    text1 = font.render("Fireboy and Watergirl" ,True, (255, 255, 255))
-    text2 = font.render("Press the space to start",True, (255, 255, 255))
+    text1 = font.render("Fireboy and Watergirl" ,True, (0, 0, 0))
+    text2 = font.render("Press the space to start",True, (0, 0, 0))
 
     while intro:
          screen.blit(intro_image,(0,0)) 
@@ -93,7 +99,7 @@ class Player:
                 self.velocityY = 0 #stops any up and down movement 
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect) #show on screen(surfac
+        surface.blit(self.image, self.rect) #show on screen(surface)
 
 
 class Player_IceCube(Player): #inheritance 
@@ -135,21 +141,11 @@ class Lands:
                     tile = (img, scaled_image)  
                     self.tile_list.append(tile) 
                 if tile == 3: 
-                    img = pygame.transform.scale(lava_img, (tile_size, 40)) 
-                    scaled_image = img.get_rect()  
-                    scaled_image.x = collum_count * tile_size 
-                    scaled_image.y = row_count * tile_size 
-                    tile = (img, scaled_image)  
-                    self.tile_list.append(tile) 
-                    #lava = Lava()  
+                    lava = Lava(collum_count * tile_size, row_count * tile_size)
+                    lava_group.add(lava)
                 if tile == 4: 
-                    img = pygame.transform.scale(water_img, (tile_size, 40)) 
-                    scaled_image = img.get_rect()  
-                    scaled_image.x = collum_count * tile_size 
-                    scaled_image.y = row_count * tile_size 
-                    tile = (img, scaled_image)  
-                    self.tile_list.append(tile)   
-                   # water = Water()
+                    water = Water(collum_count * tile_size, row_count * tile_size)
+                    water_group.add(water)
                 collum_count += 1
             row_count += 1
 
@@ -178,19 +174,26 @@ class Lands:
    def collision_detection(self, player_rect):
         for tile in self.tile_list:
             if player_rect.colliderect(tile[1]):
-                return tile[1]
+                return tile[1]   
+              
+class Water(pygame.sprite.Sprite):
+     def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame. image. load ('./water.png')
+        self.image = pygame.transform.scale(img, (tile_size, 40))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
         
-#class Lava(Lands): #inheritance 
-    #ef __init__(self, player1): 
-        #for tile in self.tile_list:
-            #if player1.colliderect(tile[1]):
-               #game_over = True
+class Lava(pygame.sprite.Sprite):
+     def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame. image. load ('./lava.png')
+        self.image = pygame.transform.scale(img, (tile_size, 40))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-#class Water(Lands): #inheritance 
-   # def __init__(self, player2): 
-      # for tile in self.tile_list:
-            #if player2.colliderect(tile[1]):
-               # game_over = True      
 
 environment_data = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -208,9 +211,13 @@ environment_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 ]
 
+lava_group = pygame.sprite.Group()
+water_group = pygame.sprite.Group()
 world = Lands(environment_data)
 
 pygame.init()
+
+game_over = False 
 clock = pygame.time.Clock()
 screen_width = 1000
 screen_height = 650
@@ -227,13 +234,13 @@ bg_img = pygame.image.load('./jungleBackground.png')
 
 player1 = Player_IceCube(10,10)
 player2 = Player_Fire(10,10)
-#dimonds = Collections(screen, player1, player2)
 
 #----------------------------------------------------
 #MOVING ROCKS
 #moving platforms for two rock images 
 platform_velocity= 3 #speed 
 platform_velocity2= 3
+
 #rock one, loading, scaling and positioning 
 moving_rock_img = pygame.image.load('./rocks.png')
 moving_rock_img = pygame.transform.scale(moving_rock_img, (tile_size, 25))
@@ -243,7 +250,25 @@ Mrock_rect = moving_rock_img.get_rect(x=100, y=100) #positioning of rock
 moving_rock2_img = pygame.image.load('./rocks.png')
 moving_rock2_img = pygame.transform.scale(moving_rock2_img, (tile_size, 25))
 Mrock2_rect = moving_rock2_img.get_rect(x=500, y=500)
+
+#instead of repeating collision detection 4 times, add the players as one varable and the rocks as one varaible 
+collision_tolerance = 10
+def moving_collision(players, rocks, collision_tolerance):
+    if players.rect.colliderect(rocks):
+        if abs(players.rect.bottom - rocks.top) < collision_tolerance: 
+            players.velocityY = 0 
+            players.rect.bottom = rocks.top #players can not move down when they hit the top of the rocks 
+            players.onGround = True
+        elif abs(players.rect.top - rocks.bottom) < collision_tolerance:
+            players.velocityY = 0
+            players.rect.top = rocks.bottom #players can not move up if it touches the bottom of the rock 
+        elif abs(players.rect.right - rocks.left) < collision_tolerance:
+            players.rect.right = rocks.left #cant move right if it hits the rocks left side 
+        elif abs(players.rect.left - rocks.right) < collision_tolerance:
+            players.rect.left = rocks.right #cant move left if hits the rocks right side 
+
 #-----------------------------------------------------
+
 #COLLECTING DIMONDS
 red_dimond = pygame.image.load('./red.dimond.png')
 red_dimond = pygame.transform.scale(red_dimond, (17,17))
@@ -264,20 +289,35 @@ blue_dimond_list = [
     pygame.Rect(50, 250, 17, 17) #left corner
 ]
 dimond_count = 0
-#-----------------------------------------------------
 
+#-----------------------------------------------------
+# GAME OVER SCREEN
+#def show_game_over(screen, dimond_count):
+    #font = pygame.font.Font (None, 32) #the font type and size 
+    #text1 = font.render( 'Game Over', True,(255,255,255)) #the text 
+    #text2 = font.render('score is', str(dimond_count), True, (255,255,255))
+    #text1_rect = text1.get_rect(center= (screen_height/2, screen_height/2)) #gets the rectangle (position) of the text 
+    #text2_rect = text2.get_rect(center= (screen_height/2, screen_height/2))
+    #gameover_screen = screen.fill(0,0,0) 
+   # screen.blit(text1, text1_rect)
+    #screen.blit(text2, text2_rect)
+    #pygame.display.flip()
+   # pygame.time.wait(4000)
+#i couldnt get the gameover screen to display so i decided to just have the game output 'game over' when the player dies. 
+#used chatGPT to tell me where to put the code (now in line 399)
+    
+#-----------------------------------------------------
+#-----------------------------------------------------
+#GAME LOOP 
 run = True
-while run:  # code of game keeps looping
+while run and not game_over:  # code of game keeps looping
     screen.blit(bg_img, (0, 0)) 
     screen.blit(moving_rock_img, Mrock_rect)
     screen.blit(moving_rock2_img, Mrock2_rect)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-    #if game_over:
-       # print("game over")
-       # break
-#-----------------------------------------------------
+#-----------------------------------------------------           
 #moving rocks, within the loop of the game so it is repeatitive 
     if Mrock_rect.left >=300 or Mrock_rect.left<100: #the rock will move from position 300 to 100
         platform_velocity*= -1 #reverses the direction 
@@ -286,8 +326,14 @@ while run:  # code of game keeps looping
     if Mrock2_rect.left >=900 or Mrock2_rect.left<500:
          platform_velocity2 *= -1
     Mrock2_rect.left += platform_velocity2
-#-----------------------------------------------------
+   
+    #had help from chatGPT with these three lines of code
+    for players in (player1, player2):
+        for rocks in (Mrock_rect, Mrock2_rect):
+             moving_collision(players, rocks, collision_tolerance)
 
+#-----------------------------------------------------
+#showing dimonds on the screen and removing them
     for R in red_dimond_list:
         screen.blit(red_dimond,(R[0], R[1]))
     for B in blue_dimond_list:
@@ -295,11 +341,39 @@ while run:  # code of game keeps looping
     for D in blue_dimond_list[:]:
         if D.colliderect(player1):
             blue_dimond_list.remove(D)
+            dimond_count+=1
     for D in red_dimond_list[:]:
         if D.colliderect(player2):
             red_dimond_list.remove(D)
+            dimond_count+=1       
+
 #-----------------------------------------------------
-    
+#lava and water colision detection  
+    # Inside the game loop
+    for lava in lava_group:
+        if player1.rect.colliderect(lava.rect):
+            game_over = True
+            break  # Exit the loop as soon as a collision is detected
+
+    for water in water_group:
+        if player2.rect.colliderect(water.rect):
+            game_over = True
+            break  # Exit the loop as soon as a collision is detected
+
+    for lava in lava_group:
+        if player2.rect.colliderect(lava.rect):
+            player2.velocityY = 0 
+            player2.rect.bottom = lava.rect.top
+            player2.onGround = True
+            break  # Break if collision is detected
+
+    for water in water_group:
+        if player1.rect.colliderect(water.rect):
+            player1.velocityY = 0 
+            player1.rect.bottom = water.rect.top
+            player1.onGround = True
+            break  # Break if collision is detected
+#-----------------------------------------------------   
 #key presses 
     #player 1
     userInput1 = pygame.key.get_pressed()
@@ -322,16 +396,45 @@ while run:  # code of game keeps looping
     if userInput2[pygame.K_SPACE]:
         player2.jump()
 #-----------------------------------------------------
+        if game_over:
 
+            font = pygame.font.Font (None, 32) #the font type and size 
+            text1 = font.render( 'Game Over', True,(255,255,255)) #the text 
+            text2 = font.render('Score: ' + str(dimond_count), True, (255, 255, 255))
+            text1_rect = text1.get_rect(center= (screen_height/2, screen_height/2 -20)) #gets the rectangle (position) of the text 
+            text2_rect = text2.get_rect(center= (screen_height/2, screen_height/2)) 
+            screen.blit(text1, text1_rect)
+            screen.blit(text2, text2_rect)
+    
+            wait_time = 4000  # 4 seconds in milliseconds
+            start_ticks = pygame.time.get_ticks()  # Get current ticks
+            waiting = True
+            while waiting:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        waiting = False
+                        run = False  # Or set to False to exit the game loop
+                if pygame.time.get_ticks() - start_ticks > wait_time:
+                    waiting = False
+                    break 
+#-----------------------------------------------------
+    lava_group.draw(screen)
+    water_group.draw(screen)
     world.draw(screen)    
-    #dimonds.draw()
-   # dimonds.update()
     player1.update(world)
     player1.draw(screen) 
     player2.update(world) #update the characters 
     player2.draw(screen) # Draw the character
     pygame.display.flip() # update the display
     clock.tick (60) 
+    
+       
+
+#-----------------------------------------------------
+#-----------------------------------------------------
 
 pygame.quit()
 sys.exit()
+
+#if players get all the dimonds in 1min then they win (add more dimonds) - need to do 
+#if they dont its gameover and they dont win 
